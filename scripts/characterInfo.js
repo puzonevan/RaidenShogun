d21a80f4c377ec0343532c6e04d71cb3ab93b023
‘Evan <puzonevan@gmail.com>
2021-09-16 23:46:10 -0700

    build section change

--- scripts/characterInfo.js
+++ scripts/characterInfo.js
@@ -44,86 +44,86 @@ const character = {
 
     builds: [
         {
-            name: "Cryo DPS Build", 
-            description: "This build focuses on empowering Ganyu's charged attack with the Wanderer's Troupe set \
-                        since it amplifies both the initial hit and the AoE explosion from each shot.",
+            name: "Electro Sub DPS Build", 
+            description: "While her main role is to be a battery and provide energy for the main damage dealer and other supports, \
+            she can still provide large burst potential and damage multiple targets at once",
             weapons: [
                 {
-                    name: "Amos' Bow", 
-                    substat: "Atk", 
-                    description: "Increases Normal Attack and Aimed Shot DMG by 12/15/18/21/24%. Increases DMG from \
-                                arrows shot by a further 8/10/12/14/16% for every 0.1s that the arrow is in flight, up \
-                                to 0.5s. Stacks up to 5 times on each arrow.",
-                    image: "./ganyu/weapons/Weapon_Amos_Bow.png"
+                    name: "Engulfing Lightning", 
+                    substat: "Energy Recharge", 
+                    description: "ATK increased by 28% of Energy Recharge over the base 100%. You can gain a maximum bonus of 80% ATK. \
+                    Gain 30% Energy Recharge for 12s after using an Elemental Burst.",
+                    image: ""
                 },
                 {
-                    name: "Prototype Crescent",
-                    substat: "Atk", 
-                    description: "Aimed Shot hits on weak points increase Movement SPD by 10% and ATK by 36/45/54/63/72% for 10s.",
-                    image: "./ganyu/weapons/Weapon_Prototype_Crescent.png"
+                    name: "The Catch",
+                    substat: "Energy Recharge", 
+                    description: "Increases Elemental Burst DMG by 16% and Elemental Burst CRIT Rate by 6%.",
+                    image: ""
                 }
             ],
             artifacts: [
                 {
-                    name: "Wanderer's Troupe", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> Elemental Mastery +80 <br><br><strong>(4)</strong> Increases Charged Attack\
-                                 DMG by 35% if the character uses a Catalyst or Bow.",
-                    image: "./ganyu/weapons/Item_Troupe_Wanderers.png"
+                    name: "Emblem of Severed Fate", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> Energy Recharge +20% <br><br><strong>(4)</strong> \
+                    Increases Elemental Burst DMG by 25% of Energy Recharge. A maximum of 75% bonus DMG can \
+                    be obtained in this way",         
+                    image: ""
                 },
                 {
-                    name: "Gladiator's Finale", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> ATK +18%",
-                    image: "./ganyu/weapons/Item_Gladiators_Nostalgia.png"
+                    name: "Emblem of Severed Fate", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> Energy Recharge +20%",
+                    image: ""
                 },
                 {
-                    name: "Blizzard Strayer", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> Cryo DMG Bonus +15%",
-                    image: "./ganyu/weapons/Item_Snowswept_Memory.png"
+                    name: "Thundering Fury", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> Electro DMG Bonus +15%",
+                    image: ""
                 }
             ]
         },
         {
-            name: "Cryo Support Build", 
-            description: "Ganyu is the premier Cryo sub DPS thanks to her high damage and uptime on her skills, even when not on the field.",
+            name: "Electro Main DPS Build", 
+            description: "The Raiden Shogun falls of in consistent damage because her auto attacks are still physical. \
+            However, her skill and ult can still dish out loads of burst damage and that is still the main focus.",
             weapons: [
                 {
-                    name: "Skyward Harp", 
-                    substat: "Crit Rate", 
-                    description: "Increases CRIT DMG by 20/25/30/35/40%. \
-                                Hits have a 60/70/80/90/100% chance to inflict a small AoE attack, \
-                                dealing 125% Physical ATK DMG. Can only occur once every 4/3.5/3/2.5/2s.",
-                    image: "./ganyu/weapons/Weapon_Skyward_Harp.png"
+                    name: "Engulfing Lightning", 
+                    substat: "Energy Recharge", 
+                    description: "ATK increased by 28% of Energy Recharge over the base 100%. You can gain a maximum bonus of 80% ATK. \
+                    Gain 30% Energy Recharge for 12s after using an Elemental Burst.",
+                    image: ""
                 },
                 {
-                    name: "The Stringless",
-                    substat: "Elemental Mastery", 
-                    description: "Increases Elemental Skill and Elemental Burst DMG by 24/30/36/42/48%.",
-                    image: "./ganyu/weapons/Weapon_The_Stringless.png"
+                    name: "Prototype Starglitter",
+                    substat: "Energy Recharge", 
+                    description: "After using an Elemental Skill, increases Normal and Charged Attack DMG by 8% for 12s. Max 2 stacks.",
+                    image: ""
                 }
             ],
             artifacts: [
                 {
-                    name: "Noblesse Oblige", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> Elemental Burst DMG +20%",
-                    image: "./ganyu/weapons/Item_Royal_Flora.png"
+                    name: "Emblem of Severed Fate", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> Energy Recharge +20% <br><br><strong>(4)</strong> \
+                    Increases Elemental Burst DMG by 25% of Energy Recharge. A maximum of 75% bonus DMG can \
+                    be obtained in this way",         
+                    image: ""
                 },
                 {
-                    name: "Blizzard Strayer", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> Cryo DMG Bonus +15%",
-                    image: "./ganyu/weapons/Item_Snowswept_Memory.png"
+                    name: "Emblem of Severed Fate", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> Energy Recharge +20%",         
+                    image: ""
                 },
                 {
-                    name: "Noblesse Oblige", 
-                    substat: "Atk% / Cryo DMG / Crit DMG",
-                    description: "<strong>(2)</strong> Elemental Burst DMG +20%<br><br>\
-                    <strong>(4)</strong> Using an Elemental Burst increase all party members' \
-                    ATK by 20% for 12s. This effect cannot stack.",
-                    image: "./ganyu/weapons/Item_Royal_Flora.png"
+                    name: "Gladiator's Finale", 
+                    substat: "Atk% / Electro DMG / Crit DMG",
+                    description: "<strong>(2)</strong> ATK +18%",
+                    image: ""
                 }
             ]
         }
